899120c3d0cb8ac575645e6c52a5bdd30d699541
Nayak-6666 <163091930+Nayak-6666@users.noreply.github.com>
2024-07-24 16:12:29 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "insta" {
+  ami                         = "ami-03972092c42e8c0ca"
+  instance_type               = "t2.small"
+  count                       = 1
+  key_name                    = "Onekey"
+  vpc_security_group_ids     = ["sg-084466caf0b08812c"]
+  subnet_id                   = "subnet-01b48833cdee67b0d"
+  associate_public_ip_address = true
+  user_data                   = "${file("data.sh")}"
+
+  tags = {
+    Name = "insta"
+  }
+}
