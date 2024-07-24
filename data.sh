a74855bf65aacacc7c53dfb8714a5ead8593b715
Nayak-6666 <163091930+Nayak-6666@users.noreply.github.com>
2024-07-24 16:35:45 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,1 @@
-sudo yum install update
-sudo yum -y install git
-sudo yum -y install python3-pip
-git clone https://github.com/Nayak-6666/python-car-prediction-1.git
-cd python-car-prediction-1
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+
