bc9c5c01a3d83598eb4ca3f4b631cb77e80d14e8
Nayak-6666 <163091930+Nayak-6666@users.noreply.github.com>
2024-07-24 16:51:29 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo yum -y update
-sudo yum -y install git
-git clone https://github.com/nisitasubudhi/fish.git
-sudo yum -y install python3-pip
-cd Fuel-Consumption-Rating
+sudo yum install git -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+cd /home
+sudo git clone https://github.com/Nayak-6666/python-car-prediction-1.git
+cd /home/python-car-prediction/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
